d11a1b8498b18ef07fc1a8c4e43475bff8fbb356
Konstantin Yavichev <kyavichev@gmail.com>
2025-03-29 20:34:32 -0700

    [next-image-app] indentation fix

--- next-image-app/src/app/components/Controls.tsx
+++ next-image-app/src/app/components/Controls.tsx
@@ -1,18 +1,18 @@
 
 
 type ControlsProps = {
-  numImages: number;
-  onLoadButtonClicked: () => void;
+	numImages: number;
+	onLoadButtonClicked: () => void;
 }
 
 const Controls = ({numImages, onLoadButtonClicked}: ControlsProps) => (
-  <>
-    <div className="stats flex items-center justify-center text-blue-100 bg-blue-400 rounded-sm">
-      0 of {numImages}
-    </div>
-    <button className="block border border-gray-200 hover:bg-gray-100 disabled:text-gray-300 disabled:bg-gray-100 rounded-sm" onClick={onLoadButtonClicked}>
-      Load More
-    </button>
-  </>
+	<>
+		<div className="stats flex items-center justify-center text-blue-100 bg-blue-400 rounded-sm">
+			0 of {numImages}
+		</div>
+		<button className="block border border-gray-200 hover:bg-gray-100 disabled:text-gray-300 disabled:bg-gray-100 rounded-sm" onClick={onLoadButtonClicked}>
+			Load More
+		</button>
+	</>
 );
 export default Controls;
