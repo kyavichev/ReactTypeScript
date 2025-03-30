cea25f1ed1ab890ff913e24a1be319bfc5021a47
Konstantin Yavichev <kyavichev@gmail.com>
2025-03-30 09:52:35 -0700

    [tic-tac-toe-adv] old code removed

--- tic-tac-toe-adv/src/components/Board.tsx
+++ tic-tac-toe-adv/src/components/Board.tsx
@@ -16,26 +16,6 @@ type BoardProps = {
 
 export default function Board({boardSize, requiredLineLength, xIsNext, squares, onPlay, winnerInfo } : BoardProps) {
 
-	function calculateWinner(squares : Array<string | null>) {
-		const lines = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6]
-		];
-		for (let i = 0; i < lines.length; i++) {
-			const [a, b, c] = lines[i];
-			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-				return {winner: squares[a], line: lines[i]};
-			}
-		}
-		return null;
-	}
-
 	function calculateWinnerAroundIndex(index: number, squares : Array<string | null>) : WinnerInfo | null {
 
 		const row = Math.floor(index / boardSize);
